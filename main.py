c3ded98166a3f3d0141c78528a95d388f837aac3
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-01-29 12:31:08 -0500

    almost done

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import random 
 
+
+def util(number_of_dices, curr_val, value_req, not_included, times_played):
+    for i in range(number_of_dices):
+        if i in not_included:
+            continue
+        numb = random.randint(1, 6)
+        print(i)
+        curr_val[i] = numb
+        times_played +=1
+        if(value_req == curr_val):
+            print(f"You did it! Took {times_played} tries")
+            break
+
+        print(f"Current Values {curr_val}")
+        keeped = list(input(f"What values would you want to keep? (1 to {number_of_dices}) "))
+        if keeped[0] == "none":
+            continue
+        else:
+            not_included.append(list(map(lambda x: x-1, map(int, keeped))))
+
 def func():
     value_req = []
     curr_val = []
+    not_included = []
     try:
         number_of_dices = int(input("How many dices would you like to play with? \n"));  
     except ValueError:
@@ -15,15 +36,29 @@ def func():
         print(f"Required values to roll are {value_req}")
         if(input("Start? (y,n) \n") == "y"):
             start = True
+            times_played = 0
             while start:
+
                 for i in range(number_of_dices):
                     numb = random.randint(1, 6)
-                    curr_val = numb
+                    curr_val.append(numb)
+                print(f"Current Values {curr_val}")
+                if(value_req == curr_val):
+                    print(f"You did it! Took {times_played} tries")
+                    break
+                keeped = list(input("What values would you want to keep?"))
+                if keeped[0] == 'none':
+                    pass
+                else:
+                    not_included.append(list(map(lambda x: x-1, map(int, keeped))))
+                util(number_of_dices, curr_val, value_req, not_included, times_played)
+
+                
                     
         else:
             return
-
-
+        
+func()
 
 
 

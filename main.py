13382e9f6fca2ba883926830197aa4f7613beaaf
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-01-29 15:12:53 +0000

    pivoting to yahtzee

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random 
 
 def func():
-    value_req = 0
-    curr_val = 0
+    value_req = []
+    curr_val = []
     try:
         number_of_dices = int(input("How many dices would you like to play with? \n"));  
     except ValueError:
@@ -10,25 +10,16 @@ def func():
         func();
     else:
         for i in range(number_of_dices):
-            value_req += random.randint(1, 6)
-        print(f"Required value to roll is {value_req}")
+            num = random.randint(1,6)
+            value_req.append(num)
+        print(f"Required values to roll are {value_req}")
         if(input("Start? (y,n) \n") == "y"):
             start = True
             while start:
-                
                 for i in range(number_of_dices):
-                    curr_val += random.randint(1, 6)
-                ans = input(f'Current Value is {curr_val}. Would you like to go again? (y,n)')
-                if ans == 'n':
-                    break;
-                elif ans == 'y':
-                    pass
-                if curr_val > value_req:
-                    print(f"You lost, getting {value_req - curr_val} over")
-                    break
-                else:
-                    print(f"You got {value_req - curr_val} close to {value_req}")
-            
+                    numb = random.randint(1, 6)
+                    curr_val = numb
+                    
         else:
             return
 

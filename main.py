5f127d1086db1e0150a30f778d2a5a85a090569f
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-01-30 00:40:12 +0000

    finish

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import random 
 
-
-def util(number_of_dices, curr_val, value_req, not_included, times_played):
-    for i in range(number_of_dices):
-        if i in not_included:
-            continue
-        numb = random.randint(1, 6)
-        print(i)
-        curr_val[i] = numb
+times_played = 1
+def util(number_of_dices, curr_val, value_req, not_included):
+    global times_played
+    r = False
+    while sorted(value_req) != sorted(curr_val):
+        for i in range(number_of_dices):
+            if i in not_included:
+                continue
+            numb = random.randint(1, 6)
+            curr_val[i] = numb
+            if(sorted(value_req) == sorted(curr_val)):
+                print(f"Current Values {curr_val}")
+                print(f"You did it! Took {times_played} tries")
+                r = True
+                break
         times_played +=1
-        if(value_req == curr_val):
-            print(f"You did it! Took {times_played} tries")
+        
+        if r:
             break
-
         print(f"Current Values {curr_val}")
-        keeped = list(input(f"What values would you want to keep? (1 to {number_of_dices}) "))
-        if keeped[0] == "none":
+        keeped = input(f"What values would you want to keep? (1 to {number_of_dices} or if mutiple type out numbers with spaces inbetween or type none) \n")
+        if keeped == 'none':
             continue
         else:
-            not_included.append(list(map(lambda x: x-1, map(int, keeped))))
+            for j in list(map(lambda x: x-1, map(int, list(keeped)))):
+                    not_included.append(j)
 
 def func():
+    global times_played
     value_req = []
     curr_val = []
     not_included = []
@@ -35,26 +43,23 @@ def func():
             value_req.append(num)
         print(f"Required values to roll are {value_req}")
         if(input("Start? (y,n) \n") == "y"):
-            start = True
-            times_played = 0
-            while start:
+            for i in range(number_of_dices):
+                numb = random.randint(1, 6)
+                curr_val.append(numb)
 
-                for i in range(number_of_dices):
-                    numb = random.randint(1, 6)
-                    curr_val.append(numb)
-                print(f"Current Values {curr_val}")
-                if(value_req == curr_val):
-                    print(f"You did it! Took {times_played} tries")
-                    break
-                keeped = list(input("What values would you want to keep?"))
-                if keeped[0] == 'none':
-                    pass
-                else:
-                    not_included.append(list(map(lambda x: x-1, map(int, keeped))))
-                util(number_of_dices, curr_val, value_req, not_included, times_played)
-
+            print(f"Current Values {curr_val}")
+            if(sorted(value_req) == sorted(curr_val)):
+                print(f"You did it! Took {times_played} tries")
+                return
+            keeped = input(f"What values would you want to keep? (1 to {number_of_dices} or if mutiple type out numbers with spaces inbetween or type none) \n")
                 
-                    
+            if keeped == 'none':
+                util(number_of_dices, curr_val, value_req, not_included)
+            else:
+                for j in list(map(lambda x: x-1, list(map(int, keeped.split())))):
+                    not_included.append(j)
+                util(number_of_dices, curr_val, value_req, not_included)
+                        
         else:
             return
         
